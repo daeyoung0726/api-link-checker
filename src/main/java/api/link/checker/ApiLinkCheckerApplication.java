c2d40935c3333fab5659557ccdbc767fddad259f
DaeYoung0726 <pdy000726@naver.com>
2024-12-20 16:09:42 +0900

    chore: first commit

---
+++ src/main/java/api/link/checker/ApiLinkCheckerApplication.java
@@ -0,0 +1,13 @@
+package api.link.checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLinkCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiLinkCheckerApplication.class, args);
+    }
+
+}
