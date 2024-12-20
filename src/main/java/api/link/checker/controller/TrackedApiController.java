bcf6f40df30e23ee1d8481b8145fd20d024b2118
DaeYoung0726 <pdy000726@naver.com>
2024-12-20 16:17:47 +0900

    feat: TrackedApiController for communication

---
+++ src/main/java/api/link/checker/controller/TrackedApiController.java
@@ -0,0 +1,44 @@
+package api.link.checker.controller;
+
+import api.link.checker.core.ApiCheckRepository;
+import api.link.checker.core.ApiInfo;
+import api.link.checker.core.SwaggerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/v1/api/link/checker")
+public class TrackedApiController {
+    private final ApiCheckRepository apiCheckRepository;
+    private final SwaggerService swaggerService;
+
+    public TrackedApiController(ApiCheckRepository apiCheckRepository,
+                                SwaggerService swaggerService) {
+        this.apiCheckRepository = apiCheckRepository;
+        this.swaggerService = swaggerService;
+    }
+
+    @GetMapping
+    public Map<String, List<ApiInfo>> getAllGroupedApis() {
+        return apiCheckRepository.getAllGroupedApis();
+    }
+
+    @GetMapping("/check")
+    public void updateCheckStatus(
+            @RequestParam("httpMethod") String httpMethod,
+            @RequestParam("path") String path,
+            @RequestParam("checked") boolean checked) {
+        apiCheckRepository.updateCheckStatus(httpMethod, path, checked);
+    }
+
+    @GetMapping("/swagger-links")
+    public Map<String, String> getSwaggerLinks() throws IOException {
+        return swaggerService.fetchSwaggerLinks(apiCheckRepository.getAllGroupedApis());
+    }
+}
